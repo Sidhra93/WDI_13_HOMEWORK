ec59ca1e6619eb6090ceba7821ad7dfa3caa8157
Sidhra <sidhra.cader@hotmail.com>
2017-08-31 12:59:26 +1000

    Updated PT Planner

--- Sidhra Cader/wk01/3-wed/PT-Planner/index.js
+++ Sidhra Cader/wk01/3-wed/PT-Planner/index.js
@@ -1,10 +1,11 @@
 // var origin = prompt("Origin:");
 // var dest = prompt("Destination:");
 
-var origin = "Southern Cross";
-var dest = "South Yarra";
+var origin = "Kooyong";
+var dest = "Flagstaff";
 var route;
 var outRoute;
+var reverseLine =[];
 
 var trainLines = [
   {
@@ -24,11 +25,18 @@ var trainLines = [
 for (var i = 0; i < trainLines.length; i++) {
   for (var j = 0; j  < trainLines[i].stops.length; j++) {
     if (origin === trainLines[i].stops[j]) {
-      console.log(trainLines[i].name + " Line: \n \n");
-      route = trainLines[i].stops.slice(j, trainLines[i].stops.indexOf(dest) + 1);
+      if (trainLines[i].stops.indexOf(origin) < trainLines[i].stops.indexOf(dest)) {
+        console.log(trainLines[i].name + " Line: \n \n");
+        route = trainLines[i].stops.slice(j, trainLines[i].stops.indexOf(dest) + 1);
+        console.log((route.join()).replace(/,/g, " -----> "));
+        console.log("\n" + trainLines[i].stops.indexOf(dest) + " stops total");
+      } else if (trainLines[i].stops.indexOf(origin) > trainLines[i].stops.indexOf(dest)) {
+        reverseLine = trainLines[i].stops.reverse();
+        console.log(trainLines[i].name + " Line: \n \n");
+        route = reverseLine.slice(reverseLine.indexOf(origin),reverseLine.indexOf(dest) + 1);
+        console.log((route.join()).replace(/,/g, " -----> "));
+        console.log("\n" + (route.indexOf(dest)) + " stops total");
+      }
     }
   }
 }
-
-console.log((route.join()).replace(/,/g, " -----> "));
-console.log("\n" + route.length + " stops total");
