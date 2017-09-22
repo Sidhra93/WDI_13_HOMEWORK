b320d0a69ceac7c560d3601a5f74b06c53422461
Sidhra <sidhra.cader@hotmail.com>
2017-09-22 17:04:36 +1000

    completed movie database logic

--- Sidhra Cader/wk04/4-thu/movies/app.rb
+++ Sidhra Cader/wk04/4-thu/movies/app.rb
@@ -4,11 +4,26 @@ require "httparty"
 require "pry"
 
 get "/" do
-  results = HTTParty.get("http://www.omdbapi.com/?t=once&apikey=2f6435d9")
-  @image = HTTParty.get("http://img.omdbapi.com/?t=once&apikey=2f6435d9")
-  @data = results.parsed_response
+
   erb :index
 end
 
+get "/results" do
+  @movie = "";
+  search = params[:search]
+  results = HTTParty.get("http://www.omdbapi.com/?s=#{search}&apikey=#{ENV["OMDB_API_KEY"]}")
+  data = results.parsed_response
+  @data_array = data["Search"]
+  erb :results
+end
+
+get '/about' do
+  movie = params[:search]
+  # binding.pry
+  results = HTTParty.get("http://www.omdbapi.com/?t=#{movie}&apikey=#{ENV["OMDB_API_KEY"]}")
+  # binding.pry
+  @data = results.parsed_response
+  erb :about
+end
 
 # binding.pry
