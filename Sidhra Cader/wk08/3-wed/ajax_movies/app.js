d14ca6aaefb98443c00207593814c6c426ec84be
Sidhra <sidhra.cader@hotmail.com>
2017-10-19 10:59:04 +1100

    ajax movies final

--- Sidhra Cader/wk08/3-wed/ajax_movies/app.js
+++ Sidhra Cader/wk08/3-wed/ajax_movies/app.js
@@ -1,12 +1,20 @@
-
 $('button').on('click', function(){
   var search = $('#search-bar').val();
-  $.ajax({
-    url:"http://www.omdbapi.com/?s=" + search +"&apikey=2f6435d9",
-    method: 'get'
-  }).done(function(response){
+
+  var options = {
+    url:"http://www.omdbapi.com",
+    method: 'get',
+    data: {s: search, apikey: '2f6435d9'}
+  };
+
+  var movieSearch = function(response){
+    $('.movie-list').html('')
     response.Search.forEach(function(movie){
-      console.log(movie.Title);
-    })
-  });
+      // console.log(movie.Title);
+      $movieTitle = $('<h2>').text(movie.Title);
+      $('.movie-list').append($movieTitle);
+    });
+  }
+
+  $.ajax(options).done(movieSearch);
 });
