29ab0626b5bb719dae9197ed40b13144f23991ba
Sidhra <sidhra.cader@hotmail.com>
2017-11-01 17:26:39 +1100

    node weather

--- Sidhra Cader/wk10/2-tue/node_weather/index.js
+++ Sidhra Cader/wk10/2-tue/node_weather/index.js
@@ -1,5 +1,5 @@
 var request = require('request');
-request('http://api.openweathermap.org/data/2.5/weather?id=7839805&APPID=d10ed7fa849100a3d43e443f9ba5b599', function (error, response, body) {
-  var jsonBody = JSON.parse(body)
+request('http://api.openweathermap.org/data/2.5/weather?id=7839805&APPID=d10ed7fa849100a3d43e443f9ba5b599&units=metric', function (error, response, body) {
+  var jsonBody = JSON.parse(body); // response comes back as string so you have to convert to JSON to be able to access values
   console.log(jsonBody.main.temp);
 });
