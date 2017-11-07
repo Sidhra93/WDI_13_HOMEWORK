2b920ee27f1aa9d1be1659b97a73f629d968023d
Sidhra <sidhra.cader@hotmail.com>
2017-11-08 05:59:25 +1100

    infinte giphy complete

--- Sidhra Cader/wk08/4-thu/infinite_giphy/app.js
+++ Sidhra Cader/wk08/4-thu/infinite_giphy/app.js
@@ -4,40 +4,28 @@ var $searchItem = $("input");
 var $searchButton = $("button");
 
 $searchButton.on('click', function(){
+  var makeAjaxRequest = function(){
+    var search = $searchItem.val();
+    var options = {
+      url: "http://api.giphy.com/v1/gifs/search",
+      method: 'get',
+      data: {q: search, api_key: APIKEY, limit: 10}
+    }
 
-  var search = $searchItem.val();
-  var options = {
-    url: "http://api.giphy.com/v1/gifs/search",
-    method: 'get',
-    data: {q: search, api_key: APIKEY, limit: 10}
-  }
-
-  $.ajax(options).done(function(response){
-    // console.log(response.data);
-    response.data.forEach(function(gif){
-      var $searchItemsDiv = $('#search-items');
-      var $img = $(`<div><img src = ${gif.images.fixed_width.url} ></div>`);
-      $searchItemsDiv.append($img);
+    $.ajax(options).done(function(response){
+      // console.log(response.data);
+      response.data.forEach(function(gif){
+        var $searchItemsDiv = $('#search-items');
+        var $img = $(`<div><img src = ${gif.images.fixed_width.url} ></div>`);
+        $searchItemsDiv.append($img);
+      })
     })
-  })
+  }
 
   $(window).scroll(function(){
     if (Math.round($(window).scrollTop() + $(window).height()) == $(document).height()) {
-      var search = $searchItem.val();
-      var options = {
-        url: "http://api.giphy.com/v1/gifs/search",
-        method: 'get',
-        data: {q: search, api_key: APIKEY, limit: 10}
-      }
-
-      $.ajax(options).done(function(response){
-        // console.log(response.data);
-        response.data.forEach(function(gif){
-          var $searchItemsDiv = $('#search-items');
-          var $img = $(`<div><img src = ${gif.images.fixed_width.url} ></div>`);
-          $searchItemsDiv.append($img);
-        })
-      })
+      makeAjaxRequest()
     }
   })
+  makeAjaxRequest()
 })
