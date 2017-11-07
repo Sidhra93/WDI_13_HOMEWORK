d80105383d1020d77175843e8ec6ac5b62a4401b
Sidhra <sidhra.cader@hotmail.com>
2017-11-08 05:56:27 +1100

    infinte giphy complete

--- Sidhra Cader/wk08/4-thu/infinite_giphy/app.js
+++ Sidhra Cader/wk08/4-thu/infinite_giphy/app.js
@@ -4,29 +4,40 @@ var $searchItem = $("input");
 var $searchButton = $("button");
 
 $searchButton.on('click', function(){
-  makeAjaxRequest();
 
-  $(window).scroll(function(){
-    if ($(window).scrollTop() == $(window).height()) {
-      makeAjaxRequest();
-    }
+  var search = $searchItem.val();
+  var options = {
+    url: "http://api.giphy.com/v1/gifs/search",
+    method: 'get',
+    data: {q: search, api_key: APIKEY, limit: 10}
+  }
+
+  $.ajax(options).done(function(response){
+    // console.log(response.data);
+    response.data.forEach(function(gif){
+      var $searchItemsDiv = $('#search-items');
+      var $img = $(`<div><img src = ${gif.images.fixed_width.url} ></div>`);
+      $searchItemsDiv.append($img);
+    })
   })
 
-  function makeAjaxRequest(){
-    var search = $searchItem.val();
-    var options = {
-      url: "http://api.giphy.com/v1/gifs/search",
-      method: 'get',
-      data: {q: search, api_key: APIKEY, limit: 10}
-    }
+  $(window).scroll(function(){
+    if (Math.round($(window).scrollTop() + $(window).height()) == $(document).height()) {
+      var search = $searchItem.val();
+      var options = {
+        url: "http://api.giphy.com/v1/gifs/search",
+        method: 'get',
+        data: {q: search, api_key: APIKEY, limit: 10}
+      }
 
-    $.ajax(options).done(function(response){
-      // console.log(response.data);
-      response.data.forEach(function(gif){
-        var $searchItemsDiv = $('#search-items');
-        var $img = $(`<div><img src = ${gif.images.fixed_width.url} ></div>`);
-        $searchItemsDiv.append($img);
+      $.ajax(options).done(function(response){
+        // console.log(response.data);
+        response.data.forEach(function(gif){
+          var $searchItemsDiv = $('#search-items');
+          var $img = $(`<div><img src = ${gif.images.fixed_width.url} ></div>`);
+          $searchItemsDiv.append($img);
+        })
       })
-    })
-  }
+    }
+  })
 })
